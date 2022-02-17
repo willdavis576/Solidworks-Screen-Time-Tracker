265f93bea20067f614c7af311f1613b632e9c50e
Will Davis <william.davis@live.co.uk>
2022-02-17 12:17:49 +0000

    Add files via upload

---
+++ run.py
@@ -0,0 +1,42 @@
+import win32gui
+import time
+
+run = False
+track = False
+timeDiff = 0
+
+with open('sessionTime.txt') as f:
+    timeDiff = eval(f.readlines()[0])
+    f.close()
+
+sessionStart = input("start session? ")
+
+if sessionStart == "y" or sessionStart == "Y":
+    run = True
+
+while run:
+    try:
+        nameID = win32gui.GetForegroundWindow()
+        progName = win32gui.GetWindowText(nameID)
+        
+        if "SOLIDWORKS" in progName and track == False:
+            clock = time.time() - timeDiff
+            track = True
+        
+        if "SOLIDWORKS" in progName and track == True:
+            timeDiff = time.time() - clock
+            
+        if "SOLIDWORKS" not in progName:
+            track = False
+            
+        print(timeDiff)
+    
+    except KeyboardInterrupt:
+        print("stopping")
+        f = open('sessionTime.txt','w')
+        f.write(str(timeDiff))
+        f.close()
+        break
+    
+        
+    
